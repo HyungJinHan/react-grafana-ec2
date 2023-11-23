0ea96fe8ee0af3e248951d679f6ef74ae1becc23
HyungJinHan <han1210_36@naver.com>
2023-11-23 17:51:36 +0900

    env 다시 지정

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import "./App.css";
 function App() {
   const iframeItems = [
     {
-      src: `http://ec2-15-165-102-138.ap-northeast-2.compute.amazonaws.com:3000/d-solo/ce979670-fbbf-4f27-9a17-e16d14468274/odn-ec2-grafana-test?orgId=1&from=1672498800000&to=1704034799999&panelId=3`,
+      src: `${process.env.REACT_APP_EC2_IP}/d-solo/ce979670-fbbf-4f27-9a17-e16d14468274/odn-ec2-grafana-test?orgId=1&from=1672498800000&to=1704034799999&panelId=3`,
       title: "odn-grafana",
       className: "clock",
     },
     {
-      src: `http://ec2-15-165-102-138.ap-northeast-2.compute.amazonaws.com:3000/d-solo/ce979670-fbbf-4f27-9a17-e16d14468274/odn-ec2-grafana-test?orgId=1&from=1672498800000&to=1704034799999&panelId=2`,
+      src: `${process.env.REACT_APP_EC2_IP}/d-solo/ce979670-fbbf-4f27-9a17-e16d14468274/odn-ec2-grafana-test?orgId=1&from=1672498800000&to=1704034799999&panelId=2`,
       title: "odn-grafana",
       className: "graph",
     },
     {
-      src: `http://ec2-15-165-102-138.ap-northeast-2.compute.amazonaws.com:3000/d-solo/ce979670-fbbf-4f27-9a17-e16d14468274/odn-ec2-grafana-test?orgId=1&from=1672498800000&to=1704034799999&panelId=1`,
+      src: `${process.env.REACT_APP_EC2_IP}/d-solo/ce979670-fbbf-4f27-9a17-e16d14468274/odn-ec2-grafana-test?orgId=1&from=1672498800000&to=1704034799999&panelId=1`,
       title: "lift-grafana",
       className: "graph",
     },
