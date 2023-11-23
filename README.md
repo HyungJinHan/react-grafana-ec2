04583053fa82cf54ae1d25d4ae56552e9f4fd44c
HyungJinHan <han1210_36@naver.com>
2023-11-23 17:09:45 +0900

    기본 리액트 템플릿에 grafana 그래프 embed (iframe)

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# react-grafana-ec2
+# Grafana (ssh)와 React 연동
+
+### 세팅 정보
+
+- AWS EC2 사용
+
+  - OS : `Ubuntu`
+  - ssh 접속은 `.pem` 키로 인증
+
+### ID / PW
+
+- ID : `admin`
+- PW : `admin`
+
+### 무한 로그인 지옥에 빠졌다면?
+
+> ssh의 `grafana.ini` 경로를 찾아 파일을 열어 설정을 변경해야 함
+>
+> 경로 : `/etc/grafana/grafana.ini` (ssh의 OS에 따라 다를 수 있음)
+
+```ini
+# enable anonymous access
+;enabled = false
+enabled = true
+
+# set to true if you host Grafana behind HTTPS. default is false.
+;cookie_secure = true
+cookie_secure = false
+
+# set cookie SameSite attribute. defaults to `lax`. can be set to "lax", "strict", "none" and "disabled"
+;cookie_samesite = lax
+cookie_samesite = none
+```
+
+### `<iframe>` 임베드 설정
+
+```ini
+# set to true if you want to allow browsers to render Grafana in a <frame>, <iframe>, <embed> or <object>. default is false.
+;allow_embedding = false
+allow_embedding = true
+```
---
+++ src/App.js
@@ -0,0 +1,37 @@
+import "./App.css";
+const baseUrl = process.env.REACT_APP_EC2_IP;
+
+function App() {
+  const iframeItems = [
+    {
+      src: `${baseUrl}/d-solo/ce979670-fbbf-4f27-9a17-e16d14468274/odn-ec2-grafana-test?orgId=1&from=1672498800000&to=1704034799999&panelId=3`,
+      title: "odn-grafana",
+      className: "clock",
+    },
+    {
+      src: `${baseUrl}/d-solo/ce979670-fbbf-4f27-9a17-e16d14468274/odn-ec2-grafana-test?orgId=1&from=1672498800000&to=1704034799999&panelId=2`,
+      title: "odn-grafana",
+      className: "graph",
+    },
+    {
+      src: `${baseUrl}/d-solo/ce979670-fbbf-4f27-9a17-e16d14468274/odn-ec2-grafana-test?orgId=1&from=1672498800000&to=1704034799999&panelId=1`,
+      title: "lift-grafana",
+      className: "graph",
+    },
+  ];
+
+  return (
+    <div className="App">
+      <div className="App-header">
+        <div className="row-grid">
+          {iframeItems.map((item, index) => (
+            // eslint-disable-next-line
+            <iframe key={index} {...item} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
