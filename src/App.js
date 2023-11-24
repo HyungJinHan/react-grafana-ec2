48cd9d1bf15988ade0a028de6f23bb04c2072073
HyungJinHan <han1210_36@naver.com>
2023-11-24 11:39:37 +0900

    readme 패널 추가

--- src/App.js
+++ src/App.js
@@ -4,31 +4,36 @@ import "./App.css";
 function App() {
   const iframeItems = [
     {
+      src: "http://ec2-15-165-102-138.ap-northeast-2.compute.amazonaws.com:3000/d-solo/ce979670-fbbf-4f27-9a17-e16d14468274/odn-ec2-grafana-test?orgId=1&from=1672498800000&to=1704034799999&panelId=5",
+      title: "readme",
+      className: "readme",
+    },
+    {
       src: `http://ec2-15-165-102-138.ap-northeast-2.compute.amazonaws.com:3000/d-solo/ce979670-fbbf-4f27-9a17-e16d14468274/odn-ec2-grafana-test?orgId=1&from=1672498800000&to=1704034799999&panelId=3`,
       title: "clock",
-      className: "small-chart",
+      className: "small",
     },
     {
       src: `http://ec2-15-165-102-138.ap-northeast-2.compute.amazonaws.com:3000/d-solo/ce979670-fbbf-4f27-9a17-e16d14468274/odn-ec2-grafana-test?orgId=1&from=1672498800000&to=1704034799999&panelId=4`,
       title: "annotation",
-      className: "small-chart",
+      className: "small",
     },
     {
       src: `http://ec2-15-165-102-138.ap-northeast-2.compute.amazonaws.com:3000/d-solo/ce979670-fbbf-4f27-9a17-e16d14468274/odn-ec2-grafana-test?orgId=1&from=1672498800000&to=1704034799999&panelId=2`,
       title: "odn-chart",
-      className: "big-chart",
+      className: "big",
     },
     {
       src: `http://ec2-15-165-102-138.ap-northeast-2.compute.amazonaws.com:3000/d-solo/ce979670-fbbf-4f27-9a17-e16d14468274/odn-ec2-grafana-test?orgId=1&from=1672498800000&to=1704034799999&panelId=1`,
       title: "lift-chart",
-      className: "big-chart",
+      className: "big",
     },
   ];
 
   return (
     <div className="App">
       <div className="App-header">
-        <div className="row-grid">
+        <div className="container">
           {iframeItems.map((item, index) => (
             // eslint-disable-next-line
             <iframe key={index} {...item} />
